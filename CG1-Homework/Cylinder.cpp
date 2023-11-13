512a08ecf37c7e9a5ea95007d90648247be28cb4
Julia Naomi <julianaomitakimaki@gmail.com>
2023-11-13 17:44:40 -0300

    correção do doesItIntercept de cylinder.cpp apenas com a lógica da interseção com a base e/ou topo faltando

--- CG1-Homework/Cylinder.cpp
+++ CG1-Homework/Cylinder.cpp
@@ -14,8 +14,10 @@ Cylinder::Cylinder(double radius, double height, std::vector<double> ambientRefl
 }
 
 
-std::optional<std::vector<double>> Cylinder::doesItIntercept(Ray Ray){
+std::optional<std::vector<double>> Cylinder::doesItIntercept(Ray Ray) {
     
+    //guardar os pontos de interseção válidos
+    std::vector<std::vector<double>> validIntersections;
     //calcular v e w 
     // v = (Po - B) - ((Po - B) . u)u
     std::vector<double> v = minusVectors(minusVectors(Ray.initialPoint, centerBottom), multiplyByScalar(axis, dotProduct(minusVectors(Ray.initialPoint, centerBottom), axis)));
@@ -24,33 +26,41 @@ std::optional<std::vector<double>> Cylinder::doesItIntercept(Ray Ray){
     
     //coeficientes da equação quadrática
     double a = dotProduct(w, w);
-    if (a == 0.0) {
-        return std::nullopt; //raio paralelo à superfície do cilindro
-    }
-
     double b = 2 * dotProduct(v, w);
     double c = dotProduct(v, v) - (radius * radius);
     double delta = (b * b) - (4.0 * a * c);
     if (delta < 0.0) {
         return std::nullopt; //sem interseção
     }
-    //parâmetros da interseção
-    double t1 = (-b + sqrt(delta)) / (2.0 * a);
-    double t2 = (-b - sqrt(delta)) / (2.0 * a);
+    if (a != 0 && delta >= 0)
+    {
+        //parâmetros da interseção
+        double t1 = (-b + sqrt(delta)) / (2.0 * a);
+        double t2 = (-b - sqrt(delta)) / (2.0 * a);
 
-    //calcular os pontos de interseção
-    std::vector<double> intersection1 = plusVectors(Ray.initialPoint, multiplyByScalar(Ray.direction, t1));
-    std::vector<double> intersection2 = plusVectors(Ray.initialPoint, multiplyByScalar(Ray.direction, t2));
+        //calcular os pontos de interseção
+        std::vector<double> intersection1 = plusVectors(Ray.initialPoint, multiplyByScalar(Ray.direction, t1));
+        std::vector<double> intersection2 = plusVectors(Ray.initialPoint, multiplyByScalar(Ray.direction, t2));
 
-    //verificar se a interseção está dentro do cilindro 
-    // 0 <= (P - B) . u <= height
+        //verificar se a interseção está dentro do cilindro 
+        // 0 <= (P - B) . u <= height
     
-    double height_u1 = dotProduct(minusVectors(intersection1, centerBottom), axis);
-    double height_u2 = dotProduct(minusVectors(intersection2, centerBottom), axis);
+        double intersection1_projection = dotProduct(minusVectors(intersection1, centerBottom), axis);
+        double intersection2_projection = dotProduct(minusVectors(intersection2, centerBottom), axis);
 
-     if ((height_u1 >= 0.0 && height_u1 <= height) || (height_u2 >= 0.0 && height_u2 <= height)) {
-        return (height_u1 >= 0.0 && height_u1 <= height) ? intersection1 : intersection2;  // Retorna o ponto de interseção válido
+        if(intersection1_projection >= 0 && intersection1_projection <= height){
+            validIntersections.push_back(intersection1);
+        }
+
+        if(intersection2_projection >= 0 && intersection2_projection <= height){
+            validIntersections.push_back(intersection2);
+        }
+
+    //ver se interceptou o topo
+    //ver se interceptou a base
+    //guardar em valid Intersections
+    //retornar o menor
+   
     }
-    return std::nullopt; //sem interseção válida
-
 }
+
