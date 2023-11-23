f0f5829d72e585acb935156ea701fdfd19791a92
alanz <alanzcc1@gmail.com>
2023-11-23 08:52:50 -0300

    Classe Matrix feita

--- CG1-Homework/Matrix.cpp
+++ CG1-Homework/Matrix.cpp
@@ -1,31 +1,53 @@
-#include "Matrix.hpp"
 
-Matrix::Matrix(int rows, int columns)
+#ifndef Matrix_HPP
+#define Matrix_HPP
+
+#include <vector>
+#include <initializer_list>
+#include <cmath>
+#include <stdexcept>
+
+
+class Matrix
 {
-    this->rows = rows;
-    this->columns = columns;
-    //std::vector<std::vector<double>> matrix(this->lines, std::vector<double>(this->columns));
-    //this->matrix = matrix;
-}
+private:
+	const size_t rows;
+	const size_t cols;
+	std::vector<std::vector<double>> elems;
+
+	public: 
+		// Constructors
+		Matrix(size_t rows, size_t cols);
+		Matrix(std::initializer_list<std::initializer_list<double>> lis);
+
+		// Destructors
+		//~Matrix();
+
+		// Gets
+		size_t GetRows() const;
+		size_t GetCols() const;
+		double &Get(size_t r, size_t c);
+		const double &Get(size_t r, size_t c) const;
+
+		// Overload +, - and * operators(friends). 
+		Matrix operator+ (const Matrix &other) const;
+		void operator+ (double scalar);
+
+		Matrix operator- (const Matrix &other) const;
+		void operator- (double scalar);
+
+		Matrix operator* (const Matrix &other) const;
+		std::vector<double> operator* (const std::vector<double> &other) const;
+		void operator* (double scalar);
+		
+		Matrix Translation(const std::vector<double> &offset);
+		Matrix RotationX(double theta);
+		Matrix RotationY(double theta);
+		Matrix RotationZ(double theta);
+		Matrix Scaling(const std::vector<double> &scale);
 
 
-Matrix Matrix::Multiplication(Matrix matrixA, Matrix matrixB)
-{
-    Matrix matrixC(matrixA.lines, matrixB.columns);
-    for(int i = 0; i < matrixA.rows; i++)
-    {
-        for(int j = 0; j < matrixB.columns; j++)
-        {
-            for(int k = 0; k < matrixA.columns; k++)
-            {
-                matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
-            }
-        }
-    }
-    return matrixC;
-}
+};
 
-Matrix::Scaling(double x, double y, double z)
-{
-    
-}
+
+#endif
